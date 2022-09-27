a88ce759c293afbf6e95dd579f9a606751bcd519
Ddanilo9 <interiordanilo3@gmail.com>
2022-09-27 15:37:14 +0200

    add petToy/PetAccessori

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 include_once __DIR__ . '/app/PetFood.php';
-
-
+include_once __DIR__ . '/app/PetToy.php';
+include_once __DIR__ . '/app/PetAccecssory.php';
 
 
 $name = 'Biscotti di pollo';
@@ -18,9 +18,50 @@ $name = 'Carne agnello in scatola';
 $price = 1.50;
 $brand = 'Monge';
 $DateExpiration = '11/05/2023';
-$weight = '1.5';
+$weight = 1.5;
 
 $carneAgnello = new PetFood($name, $price, $brand, $DateExpiration, $weight);
 
 var_dump($biscottiPollo);
 var_dump($carneAgnello);
+
+$name = 'Osso di legno Java';
+$price = 4.90;
+$brand = 'Imac';
+$ForSizeAnimal = [];
+$materials = 'legno';
+
+$ossoLegno = new PetToy($name, $price, $brand, $ForSizeAnimal, $materials);
+
+$name = 'Palla Intrecciata in Corda';
+$price = 3.00;
+$brand = 'Imac';
+$ForSizeAnimal = [];
+$materials = 'Cotone naturale';
+
+$PallaIntrecciata = new PetToy($name, $price, $brand, $ForSizeAnimal, $materials);
+
+
+var_dump($ossoLegno);
+var_dump($PallaIntrecciata);
+
+
+$name = 'Cappottino Frozen';
+$price = 44.00;
+$brand = 'Musi';
+$size = 'S';
+$color = 'black';
+
+$Cappottino = new PetAccessory($name, $price, $brand, $size, $color);
+
+$name = 'Maglioncino Gomitolo';
+$price = 40.00;
+$brand = 'Musi';
+$size = 'M';
+$color = 'red';
+
+$Maglioncino = new PetAccessory($name, $price, $brand, $size, $color);
+
+
+var_dump($Cappottino);
+var_dump($Maglioncino);
